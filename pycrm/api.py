920d00f512f343bd73d9a619964d64d8f3db7cb3
wangchunlei <wcl_orange@163.com>
2015-03-28 23:11:56 +0800

    add post api {name is primary}

--- pycrm/api.py
+++ pycrm/api.py
@@ -1,6 +1,26 @@
 import frappe
+import json
+
 
 @frappe.whitelist()
-def get_logged_user():
-	u = frappe.db.sql("select * from tabcustomer", as_dict=True)[0]
-	return u.cus_name
+def query():
+    # u = frappe.db.sql("select * from tabcustomer", as_dict=True)[0]
+    u = frappe.db.exists("customer", "d906c05bf9")
+    print u
+    return u
+
+
+@frappe.whitelist()
+def newcustomer():
+    inputdata = frappe.local.request.stream.readlines()
+
+    data = json.loads(inputdata[0])
+    name = data["name"]
+    if frappe.db.exists("customer", name):
+        return "already exists recode with name is " + name
+    data.update({"doctype": "customer"})
+
+    frappe.local.response.update(
+        {"data": frappe.get_doc(data).insert().as_dict()})
+
+    frappe.db.commit()
