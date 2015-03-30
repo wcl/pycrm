afd3876b89aebd6a0134535773746ac527a0fe19
pubby <wcl_orange@163.com>
2015-03-30 15:20:58 +0800

    n

--- pycrm/api.py
+++ pycrm/api.py
@@ -6,15 +6,17 @@ import json
 def query():
     # u = frappe.db.sql("select * from tabcustomer", as_dict=True)[0]
     # u = frappe.db.exists("customer", "d906c05bf9")
+    print frappe.local.form_dict.keys()
     return 123
 
 
 @frappe.whitelist()
 def newcustomer():
     inputdata = frappe.local.request.stream.readlines()
-
-    data = json.loads(inputdata[0])
-    name = data["name"]
+    name = ""
+    if inputdata:
+    	data = json.loads(inputdata[0])
+    	name = data["name"]
     if frappe.db.exists("customer", name):
         return "already exists recode with name is " + name
     data.update({"doctype": "customer"})
