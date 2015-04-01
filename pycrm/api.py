1a932fe68e9327449bfa968e0d7c1a204812c554
pubby <wcl_orange@163.com>
2015-04-01 17:49:26 +0800

    new image

--- pycrm/api.py
+++ pycrm/api.py
@@ -25,3 +25,27 @@ def newcustomer():
         {"data": frappe.get_doc(data).insert().as_dict()})
 
     frappe.db.commit()
+
+
+@frappe.whitelist()
+def cancel():
+    pass
+
+@frappe.whitelist()
+def uploadfile():
+    try:
+        if frappe.form_dict.get('from_form'):
+            try:
+                ret = frappe.utils.file_manager.upload()
+            except frappe.DuplicateEntryError:
+                # ignore pass
+                ret = None
+                frappe.db.rollback()
+        else:
+            if frappe.form_dict.get('method'):
+                ret = frappe.get_attr(frappe.form_dict.method)()
+    except Exception:
+        frappe.errprint(frappe.utils.get_traceback())
+        ret = None
+    frappe.db.set("customer","cus_image",ret["file_url"])
+    return ret
