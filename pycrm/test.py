5b68b724f25db5664cc9e893b59d93efff89c111
wangcl <wangcl@wangcldeMBP.lan>
2015-05-13 00:16:46 +0800

    cccc

---
+++ pycrm/test.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-  
+import frappe
+import json
+import logging
+import time,datetime
+#UnicodeDecodeError: 'ascii' codec can't decode byte 0xe5 in position 1: ordinal not in range(128)
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+logging.basicConfig(filename="..//logs//api.log",level=logging.DEBUG)
+
+@frappe.whitelist(allow_guest=True)
+def query():
+    # u = frappe.db.sql("select * from tabcustomer", as_dict=True)[0]
+    # u = frappe.db.exists("customer", "d906c05bf9")
+    # print frappe.local.form_dict.keys()
+    u = frappe.db.get_value("Customer", {"name": ("='o6j3_stFjtOq97wFtT4Y95VNak1E'")})
+
+    return u
