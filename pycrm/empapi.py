0e10b57c73740b63c7bab43e79f42a961148a368
wangcl <wangcl@wangcldeMBP.lan>
2015-05-13 13:27:55 +0800

    autoupdate

--- pycrm/empapi.py
+++ pycrm/empapi.py
@@ -23,32 +23,28 @@ def setEmployeeWXID():
                 datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')
             logging.debug(currentTime+"inputdata[0]="+inputdata[0])
             data = json.loads(inputdata[0])
-            em_Name = data["em_Name"].encode('utf-8')
+            em_Name = data["em_Name"]
             data["em_Name"]=em_Name
             em_Mobile = data["em_Mobile"]
             em_WXID = data["em_WXID"]
-            logging.debug(
-                currentTime+"em_Name={0},em_Mobile={1},em_WXID={2}".format(em_Name, em_Mobile, em_WXID))
-
-            empDoc = frappe.get_doc(
-                "Employee", {"em_Mobile": em_Mobile, "em_Name": em_Name})
-
+            logging.debug(currentTime+"em_Name={0},em_Mobile={1},em_WXID={2}".format(em_Name, em_Mobile, em_WXID))
+            empDoc = frappe.get_doc("Employee", {"em_Mobile": em_Mobile, "em_Name": em_Name})
             if empDoc != None:
                 empDoc.update(data)
                 empDoc.save()
                 if empDoc.as_dict()["em_Enabled"] == 1:
-                    message = "已经审核您为公司销售"
+                    message = "注册成功"
                 else:
-                    message = "还未审核您为公司销售"
+                    message = "注册成功，请联系管理员核查"
                 frappe.local.response.update(
                     {"state": "update", "message": message})
             else:
                 data.update({"doctype": "Employee"})
                 data["em_Enabled"] = 0
                 frappe.get_doc(data).insert()
-                message = "请等待公司审核"
+                message = "您是新注册，请联系管理员核查"
                 frappe.local.response.update(
                     {"state": "insert", "message": message})
             frappe.db.commit()
     except:
-        logging.exception(currentTime)
+        #logging.exception(currentTime)
