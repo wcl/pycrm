a571de41b5e868aff8d1762c54b234241d4d01c8
wangcl <wcl_orange@163.com>
2015-05-16 19:35:43 +0800

    cccc

--- pycrm/pycrm/doctype/employee/employee.py
+++ pycrm/pycrm/doctype/employee/employee.py
@@ -15,32 +15,38 @@ import datetime
 logging.basicConfig(filename="..//logs//api.log",
                     level=logging.DEBUG, format='%(asctime)s %(message)s')
 import re
+
+
 class Employee(Document):
-	def before_insert(self):
-                logging.debug("编码={0},手机号={1}".format(self.em_Code,self.em_Mobile))
-		#验证编码
-		code=self.em_Code
-		codeIsint=True
-		try:
-                    code=int(code)
-                    codeIsint=isinstance(code,int)
-                except:
-                    codeIsint=False
-		if codeIsint:
-		    if int(self.em_Code)>9999 or int(self.em_Code)<1:
-			message="编码应为1至9999之间的整数"
-			raise Exception(message)
-		else:
-		    message="您输入的不是整数，编码应为1至9999之间的整数"
-		    logging.debug(message)
-		    raise Exception(message)
-		#验证手机号
-		if re.search('1\d{10}', self.em_Mobile)==None:
-		    message="手机号不合法,请重新录入"
-		    logging.debug(message)
-		    raise Exception(message)
-		#验证邮箱
-                if re.search('\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*', self.em_Email)==None:
-		    message="电子邮箱不合法,请重新录入"
-		    logging.debug(message)
-		    raise Exception(message)       
+
+    def before_insert(self):
+        logging.debug(
+            "编码={0},手机号={1}".format(self.em_Code, self.em_Mobile))
+        # 验证编码
+        code = self.em_Code
+        codeIsint = True
+        try:
+            code = int(code)
+            codeIsint = isinstance(code, int)
+        except:
+            codeIsint = False
+        if codeIsint:
+            if int(self.em_Code) > 9999 or int(self.em_Code) < 1:
+                message = "编码应为1至9999之间的整数"
+                raise Exception(message)
+        else:
+            message = "您输入的不是整数，编码应为1至9999之间的整数"
+            logging.debug(message)
+            raise Exception(message)
+        # 验证手机号
+        if re.search('1\d{10}', self.em_Mobile) == None:
+            message = "手机号不合法,请重新录入"
+            logging.debug(message)
+            raise Exception(message)
+        # 验证邮箱
+
+        logging.debug(type(self.em_Email))
+        if re.search('\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*', str(self.em_Email)) == None:
+            message = "电子邮箱不合法,请重新录入"
+            logging.debug(message)
+            raise Exception(message)
